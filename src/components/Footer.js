75afe0a7feb37f76724b777d17a7bcb1f9e3753d
unknown <harshdhande984@gmail.com>
2022-11-22 00:55:01 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <div class="card text-center">
+  <div class="card-header">
+    Created By @Harsh_Dhande
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Features of TextUtils</h5>
+    <p class="card-text">Click on Button and Make Changes According You.</p>
+   
+  </div>
+  
+</div>
+    </div>
+  )
+}
---
+++ src/components/TextForm.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react'
+
+
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        //console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase()
+        setText(newText)
+    }
+
+    const handleLoClick = ()=>{
+        //console.log("Lowecase was clicked" + text);
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+
+    const handleClearClick = ()=>{
+        //console.log("Lowecase was clicked" + text);
+        let newText = ''
+        setText(newText)
+    }
+
+    const handleCopy = ()=>{
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+
+    }
+
+    const handleExtraSpace = ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
+    const handleOnChange = (event)=>{
+        console.log("On change");
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState('');
+  return (
+    <>
+    <div className="container">
+       <h1>{props.heading}</h1>
+       <div className="mb-3">
+       <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+       </div>
+       <button className="btn btn-dark mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+       <button className="btn btn-dark mx-2" onClick={handleLoClick}>Convert to Lowecase</button>
+       <button className="btn btn-dark mx-2" onClick={handleClearClick}>Clear Text</button>
+       <button className="btn btn-dark mx-2" onClick={handleCopy}>Copy Text</button>
+       <button className="btn btn-dark mx-2" onClick={handleExtraSpace}>Remove Extra Space</button>
+    </div>
+
+    <div className="container my-2">
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+  )
+}
